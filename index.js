6119030a0f73c54bb6e1a22c199050dcbc8767ce
Fedot1k <frasssepkin@gmail.com>
2024-09-05 23:01:37 +0300

    light speed

--- index.js
+++ index.js
@@ -6,22 +6,75 @@ import { textData, buttonData, errorData } from "./watcher.js";
 
 const bot = new TelegramBot(TelegramToken, { polling: true });
 const FedotID = 870204479;
+const DavidID = 923690530;
 
 let usersData = [];
 
 let introText = `Добрo пожаловать в <b>Нейросетивичок</b>. Чтобы задать вопрос, напишите его в чате.\n\n<b>Команды:</b>\n<blockquote>/start - перезапуск\n/reset - сброс контекста\n/support - поддержка\n/about - о боте</blockquote>`;
-let aboutText = `Что такое <b>Нейросетивичок?</b>\n<blockquote>Бот, разработанный компанией digfusion с использованием OpenAI API.</blockquote>`;
+
+let aboutText = `<b>Что такое Нейросетивичок?</b>\n<blockquote><b>Бот</b>, разработанный компанией <b>digfusion</b> с использованием <b>Hugging Face API</b>.</blockquote>\n\n<b>Модели искусственного интеллекта:</b>\n<blockquote><b>• OpenGPT 4o</b> - Текстовые запросы\n<b>• OpenGPT 4o</b> - Генерация изображений\n<b>• Instant Video</b> - Генерация видео</blockquote>\n\n<b>Отсутствие ограничений:</b>\n<blockquote><b>Главное преимущество digfusion - Открытость.</b>\n\nМы становимся лучше и не лимитируем количество запросов.</blockquote>`;
+
 let supportText = `@digfusionsupport\n\nДавид | с 10:00 до 21:00\n\n@digfusion × 2024`;
 
 bot.setMyCommands([
   { command: "/start", description: "Перезапуск" },
-  { command: "/text", description: "Чат бот" },
+  { command: "/reset", description: "Сброс контекста" },
+  { command: "/text", description: "Текст" },
   { command: "/image", description: "Изображение" },
   { command: "/video", description: "Видео" },
-  { command: "/reset", description: "Сброс контекста" },
   { command: "/support", description: "Поддержка " },
   { command: "/about", description: "О боте" },
 ]);
+
+async function changeMode(chatId, mode) {
+  const dataAboutUser = usersData.find((obj) => obj.chatId == chatId);
+
+  try {
+    switch (mode) {
+      case `response`:
+        await bot
+          .sendMessage(chatId, `Генерация текста ✅`, {
+            parse_mode: `HTML`,
+            disable_web_page_preview: true,
+            reply_markup: {
+              inline_keyboard: [[]],
+            },
+          })
+          .then((message) => {
+            dataAboutUser.userAction = `response`;
+          });
+        break;
+      case `image`:
+        await bot
+          .sendMessage(chatId, `Генерация изображений ✅`, {
+            parse_mode: `HTML`,
+            disable_web_page_preview: true,
+            reply_markup: {
+              inline_keyboard: [[]],
+            },
+          })
+          .then((message) => {
+            dataAboutUser.userAction = `image`;
+          });
+        break;
+      case `video`:
+        await bot
+          .sendMessage(chatId, `Генерация видео ✅`, {
+            parse_mode: `HTML`,
+            disable_web_page_preview: true,
+            reply_markup: {
+              inline_keyboard: [[]],
+            },
+          })
+          .then((message) => {
+            dataAboutUser.userAction = `video`;
+          });
+        break;
+    }
+  } catch (error) {
+    errorData(chatId, dataAboutUser.login, `${String(error)}`);
+  }
+}
 
 async function first(chatId, stage = 1, about = false) {
   const dataAboutUser = usersData.find((obj) => obj.chatId == chatId);
@@ -41,6 +94,16 @@ async function first(chatId, stage = 1, about = false) {
         await bot.sendPhoto(chatId, supportImagePath, { caption: supportText });
         break;
     }
+  } catch (error) {
+    errorData(chatId, dataAboutUser.login, `${String(error)}`);
+  }
+}
+
+async function getResponse(chatId, userPrompt) {
+  const dataAboutUser = usersData.find((obj) => obj.chatId == chatId);
+
+  console.log(`starting response`);
+  try {
   } catch (error) {
     errorData(chatId, dataAboutUser.login, `${String(error)}`);
   }
@@ -72,10 +135,11 @@ async function getVideo(chatId, userPrompt) {
   try {
     const space = await Client.connect("KingNish/Instant-Video");
     const result = await space.predict("/instant_video", {
-      instruction: userPrompt,
+      prompt: userPrompt,
     });
 
     console.log(result.data);
+    await bot.sendVideo(chatId, result.data[0].video.url);
   } catch (error) {
     errorData(chatId, dataAboutUser.login, `${String(error)}`);
   }
@@ -83,7 +147,7 @@ async function getVideo(chatId, userPrompt) {
 
 async function StartAll() {
   bot.on(`message`, async (message) => {
-    if (FedotID == message.chat.id) {
+    if (FedotID == message.chat.id || DavidID == message.chat.id) {
       let text = message.text;
       let chatId = message.chat.id;
       let userMessage = message.message_id;
@@ -100,13 +164,13 @@ async function StartAll() {
             first(chatId, 1);
             break;
           case `/text`:
-            first(chatId, 1);
+            changeMode(chatId, `response`);
             break;
           case `/image`:
-            first(chatId, 1);
+            changeMode(chatId, `image`);
             break;
           case `/video`:
-            first(chatId, 1);
+            changeMode(chatId, `video`);
             break;
           case `/reset`:
             first(chatId, 1);
@@ -120,7 +184,7 @@ async function StartAll() {
         }
         if (Array.from(text)[0] != "/") {
           switch (dataAboutUser.userAction) {
-            case `text`:
+            case `response`:
               getResponse(chatId, text);
               break;
             case `image`:
